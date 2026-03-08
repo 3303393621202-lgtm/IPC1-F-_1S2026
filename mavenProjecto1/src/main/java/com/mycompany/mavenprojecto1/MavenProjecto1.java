ab350357bbbbe48c5d1600cfa5115860181cc1d7
Rodrigo Gonzalez <3303393621202@ingenieria.usac.edu.gt>
2026-03-08 15:03:49 -0600

    this is the first commit

---
+++ mavenProjecto1/src/main/java/com/mycompany/mavenprojecto1/MavenProjecto1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenprojecto1;
+
+/**
+ *
+ * @author ACER
+ */
+public class MavenProjecto1 {
+
+    public static void main(String[] args) {
+        boolean boleano ;  
+        boleano = false ; //solo admite TRUE o FALSE
+        System.out.println("boleano = " + boleano);
+    }
+}
