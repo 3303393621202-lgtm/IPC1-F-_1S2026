029cfaed5fd48cefa22c086e18c0413ed2fe1436
Rodrigo Gonzalez <3303393621202@ingenieria.usac.edu.gt>
2026-03-12 15:36:04 -0600

    tercer commit

--- mavenProjecto1/src/main/java/com/mycompany/mavenprojecto1/MavenProjecto1.java
+++ mavenProjecto1/src/main/java/com/mycompany/mavenprojecto1/MavenProjecto1.java
@@ -1,23 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.mavenprojecto1;
 
-/**
- *
- * @author ACER
- */
 public class MavenProjecto1 {
 
     public static void main(String[] args) {
-        boolean boleano ;  
-        boleano = false ; //solo admite TRUE o FALSE
-        System.out.println("boleano = " + boleano);
-        
-        float flotante ;   //permite ecimales con casting
-        flotante = 2000000 ;
-        System.out.println("flotante = " + flotante);
         
         System.out.println("Imagina que tienes dos vacas...");
                 
---
+++ mavenProjecto1/src/main/java/com/mycompany/mavenprojecto1/menuPrincipal.java
@@ -0,0 +1,129 @@
+
+package com.mycompany.mavenprojecto1;
+
+
+public class menuPrincipal extends javax.swing.JFrame {
+    
+    //iniciar los vectores
+    String[] codigo = new String [100];
+    String[] nombre = new String [100];
+    double[] precio = new double [100];
+    int[] stock = new int [100];
+    int contador = 0;
+
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(menuPrincipal.class.getName());
+
+    
+    public menuPrincipal() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        agregar = new javax.swing.JButton();
+        buscar = new javax.swing.JButton();
+        eliminar = new javax.swing.JButton();
+        registrar = new javax.swing.JButton();
+        generarRep = new javax.swing.JButton();
+        datosEstu = new javax.swing.JButton();
+        salida = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Gestion de Inventario");
+
+        agregar.setText("Agregar producto");
+        agregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                agregarActionPerformed(evt);
+            }
+        });
+
+        buscar.setText("Buscar producto");
+
+        eliminar.setText("Eliminar producto");
+
+        registrar.setText("Registrar venta");
+
+        generarRep.setText("Generar reportes");
+
+        datosEstu.setText("Ver datos del estudiante");
+
+        salida.setText("Salir");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(agregar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(buscar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(eliminar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE)
+                    .addComponent(registrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(datosEstu)
+                        .addComponent(generarRep, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(salida))
+                .addContainerGap(36, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(141, 141, 141))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(agregar)
+                    .addComponent(generarRep))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buscar)
+                    .addComponent(datosEstu))
+                .addGap(18, 18, 18)
+                .addComponent(eliminar)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(registrar)
+                    .addComponent(salida))
+                .addGap(66, 66, 66))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void agregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregarActionPerformed
+    //Agregar producto
+    agregarProducto ventana = new agregarProducto();
+    ventana.setVisible(true);
+    
+    }//GEN-LAST:event_agregarActionPerformed
+
+    
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(() -> new menuPrincipal().setVisible(true));
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton agregar;
+    private javax.swing.JButton buscar;
+    private javax.swing.JButton datosEstu;
+    private javax.swing.JButton eliminar;
+    private javax.swing.JButton generarRep;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton registrar;
+    private javax.swing.JButton salida;
+    // End of variables declaration//GEN-END:variables
+}
